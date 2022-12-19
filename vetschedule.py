eaf5f47d22a908b24b33699e75ee858126e50bfa
Metranohm <andrewwink@gmail.com>
2022-12-18 18:30:44 -0600

    food menu

--- vetschedule.py
+++ vetschedule.py
@@ -11,15 +11,27 @@ inventory["rum"] = 8.00
 inventory["tequila"] = 10.00
 inventory["ansinthe"] = 15.00
 inventory["mescal"] = 12.00
+inventory["fish & chips"] = 10.00,
+inventory["chicken wings"] = 8.00
+inventory["chicken tenders"] = 8.00
+inventory["sour patch kids"] = 5.00
+
+# Print the available items and prices
+print("Welcome to the bar! Here are the items available for purchase:")
+for name, price in inventory.items():
+  print(f"{name}: ${price:.2f}")
 
 # Loop until the user is finished entering items
 while True:
-  # Get the name and price of the item from the user
-  name = input("Enter the name of the item: ")
-  price = float(input("Enter the price of the item: "))
+  # Get the name of the item from the user
+  name = input("Enter the name of the item you would like to purchase: ")
   
-  # Add the price to the tab
-  tab += price
+  # Check if the item is in the inventory
+  if name in inventory:
+    # Add the price of the item to the tab
+    tab += inventory[name]
+  else:
+    print("Sorry, that item is not available.")
   
   # Check if the user is finished entering items
   finished = input("Are you finished entering items (Y/N)? ")
